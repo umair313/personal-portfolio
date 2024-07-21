79e44d0fd2fa1296d71de8e24569492715d892c4
umair <umairapi1995@gmail.com>
2024-07-21 15:23:30 +0500

    changed resume link

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="https://drive.google.com/file/d/1XlIqP29wNuign84Js-jddYCavRyQUGwt/view" className="resume-link">
               Resume
             </a>
           </nav>
